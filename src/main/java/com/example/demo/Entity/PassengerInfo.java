8737f2cf47cc71d9bdb8c430b9280d36904ce84c
Abuu001 <abulugz@gmail.com>
2021-06-09 00:30:27 +0300

    Added Passenger props

--- src/main/java/com/example/demo/Entity/PassengerInfo.java
+++ src/main/java/com/example/demo/Entity/PassengerInfo.java
@@ -1,2 +1,33 @@
-package com.example.demo.Entity;public class PassengerInfo {
+package com.example.demo.Entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "passenger_info")
+public class PassengerInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long pId;
+    private String name;
+    private String email;
+    private Character gender;
+    private String source;
+    private String destination;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy")
+    private Date travelDate;
+    private String pickupTime;
+    private String arrivalTime;
+    private double fare;
 }
