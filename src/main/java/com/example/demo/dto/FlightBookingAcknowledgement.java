8e096140014040d6c978a201d6c8f1a3385ac32f
Abuu001 <abulugz@gmail.com>
2021-06-09 00:33:42 +0300

    md

--- src/main/java/com/example/demo/dto/FlightBookingAcknowledgement.java
+++ src/main/java/com/example/demo/dto/FlightBookingAcknowledgement.java
@@ -1,2 +1,20 @@
-package com.example.demo.dto;public class FlightBookingAcknowledgement {
+package com.example.demo.dto;
+
+import com.example.demo.Entity.PassengerInfo;
+import com.example.demo.Entity.PaymentInfo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FlightBookingAcknowledgement {
+
+    private String status;
+    private double totalFare;
+    private String pnrNo;
+    private PassengerInfo passengerInfo;
+
 }
--- src/main/java/com/example/demo/dto/FlightBookingRequest.java
+++ src/main/java/com/example/demo/dto/FlightBookingRequest.java
@@ -1,2 +1,18 @@
-package com.example.demo.dto;public class FlightBookingRequest {
+package com.example.demo.dto;
+
+import com.example.demo.Entity.PassengerInfo;
+import com.example.demo.Entity.PaymentInfo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FlightBookingRequest {
+
+    private PassengerInfo passengerInfo;
+
+    private PaymentInfo paymentInfo;
 }
