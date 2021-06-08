cd215f9f42f6753a20cd1667c4b6d093ce42ed93
Abuu001 <abulugz@gmail.com>
2021-06-09 00:31:48 +0300

    Repositories

--- src/main/java/com/example/demo/Repository/PassengerInfoRepository.java
+++ src/main/java/com/example/demo/Repository/PassengerInfoRepository.java
@@ -1,2 +1,10 @@
-package com.example.demo.Repository;public interface PassengerInfoRepository {
+package com.example.demo.Repository;
+
+import com.example.demo.Entity.PassengerInfo;
+import jdk.jfr.Registered;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PassengerInfoRepository extends JpaRepository<PassengerInfo,Long> {
 }
--- src/main/java/com/example/demo/Repository/PaymentInfoRepository.java
+++ src/main/java/com/example/demo/Repository/PaymentInfoRepository.java
@@ -1,2 +1,9 @@
-package com.example.demo.Repository;public interface PaymentInfoRepository {
+package com.example.demo.Repository;
+
+import com.example.demo.Entity.PaymentInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentInfoRepository extends JpaRepository<PaymentInfo,Integer> {
 }
