0bc6201fef907e767e2bb3b90865dd92c07216f6
Abuu001 <abulugz@gmail.com>
2021-06-08 21:59:31 +0300

    Department properties

--- src/main/java/com/example/demo/Entity/Department.java
+++ src/main/java/com/example/demo/Entity/Department.java
@@ -1,2 +1,23 @@
-package com.example.demo.Entity;public class Department {
+package com.example.demo.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long departmentId;
+    private String departmentName;
+
+    @Column(name = "dep_code")
+    private String getDepartmentCode;
 }
