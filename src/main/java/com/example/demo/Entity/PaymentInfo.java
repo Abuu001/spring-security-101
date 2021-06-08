7c5a7163cf07e7642dbadd1299c663f626931767
Abuu001 <abulugz@gmail.com>
2021-06-09 00:30:52 +0300

    Added payment props

--- src/main/java/com/example/demo/Entity/PaymentInfo.java
+++ src/main/java/com/example/demo/Entity/PaymentInfo.java
@@ -1,2 +1,24 @@
-package com.example.demo.Entity;public class PaymentInfo {
+package com.example.demo.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name = "payment_info")
+@NoArgsConstructor
+public class PaymentInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer paymentId;
+    private  String accountNo;
+    private double amount;
+    private String cardType;
+    private Long passengerId;
+
 }
