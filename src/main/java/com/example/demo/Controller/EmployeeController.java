b143b1fba8f21f83fb7826275608efd6c2a7ed5f
Abuu001 <abulugz@gmail.com>
2021-06-08 21:59:01 +0300

    Employee rest API

--- src/main/java/com/example/demo/Controller/EmployeeController.java
+++ src/main/java/com/example/demo/Controller/EmployeeController.java
@@ -1,2 +1,31 @@
-package com.example.demo.Controller;public class EmployeeController {
+package com.example.demo.Controller;
+
+import com.example.demo.Entity.Employee;
+import com.example.demo.Service.EmployeeService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/employee")
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping("/newemployee")
+    public Employee saveEmployee(@RequestBody Employee newEmployee){
+        return employeeService.saveEmployee(newEmployee);
+    }
+
+    @GetMapping("/all")
+    public List<Employee> getAllEmployees(){
+        return employeeService.allEmployees();
+    }
+
 }
