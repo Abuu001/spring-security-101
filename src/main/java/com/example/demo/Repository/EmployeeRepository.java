424180d50a8ed24504dd7879523295a357a7e2ce
Abuu001 <abulugz@gmail.com>
2021-06-08 22:01:17 +0300

    Employee  Repository

--- src/main/java/com/example/demo/Repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/Repository/EmployeeRepository.java
@@ -1,2 +1,9 @@
-package com.example.demo.Repository;public interface EmployeeRepository {
+package com.example.demo.Repository;
+
+import com.example.demo.Entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 }
