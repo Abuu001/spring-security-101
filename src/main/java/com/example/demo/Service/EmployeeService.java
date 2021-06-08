31863ed65c603401cbc83d3ac5b170d0f2e9a55a
Abuu001 <abulugz@gmail.com>
2021-06-08 22:01:48 +0300

    Employee service

--- src/main/java/com/example/demo/Service/EmployeeService.java
+++ src/main/java/com/example/demo/Service/EmployeeService.java
@@ -1,2 +1,27 @@
-package com.example.demo.Service;public class EmployeeService {
+package com.example.demo.Service;
+
+import com.example.demo.Entity.Employee;
+import com.example.demo.Repository.DepartmentRepository;
+import com.example.demo.Repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Employee saveEmployee(Employee newEmployee) {
+        return employeeRepository.save(newEmployee);
+    }
+
+    public List<Employee> allEmployees() {
+        return employeeRepository.findAll();
+    }
 }
