b943e183e8c9cf3b7b5f3bab3496eb2ab1b3c968
Abuu001 <abulugz@gmail.com>
2021-06-08 21:59:53 +0300

    Employee properties

--- src/main/java/com/example/demo/Entity/Employee.java
+++ src/main/java/com/example/demo/Entity/Employee.java
@@ -1,2 +1,47 @@
-package com.example.demo.Entity;public class Employee {
+package com.example.demo.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long employeeId;
+
+    @Column(name = "employee_name")
+    private String empName;
+    private String email;
+
+    @Autowired
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Department> department;
+
+    @Column(name ="dob")
+    private LocalDate dateOfBirth;
+
+    @Transient
+    private Integer age;
+
+    public Integer getAge() {
+
+        //current date
+        LocalDate now = LocalDate.now();
+
+        Period period = Period.between(dateOfBirth,now);
+
+        return period.getYears();
+    }
 }
