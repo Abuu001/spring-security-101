0a820d81c7c3f39efd774ed2fe58fad526f80b74
Abuu001 <abulugz@gmail.com>
2021-06-09 00:33:18 +0300

    Handles insufficient balance exception

--- src/main/java/com/example/demo/Utils/PaymentUtils.java
+++ src/main/java/com/example/demo/Utils/PaymentUtils.java
@@ -1,2 +1,29 @@
-package com.example.demo.Utils;public class PaymentUtils {
+package com.example.demo.Utils;
+
+import com.example.demo.Exception.InsufficientAmountException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PaymentUtils {
+
+    private static Map<String,Double> paymentMap =new HashMap<>();
+
+    static{
+        paymentMap.put("account1" ,12000.0d);
+        paymentMap.put("account2" ,200.55d);
+        paymentMap.put("account3" ,890.45d);
+        paymentMap.put("account4" ,5000.20d);
+        paymentMap.put("account5" ,760.04d);
+        paymentMap.put("account6" ,8000.0d);
+
+    }
+
+    public static boolean validateCreditLimit(String accNo,double paidAmount){
+        if(paidAmount>paymentMap.get(accNo)){
+            throw new InsufficientAmountException("insufficient funds !!! ⚠⚠⚠");
+        }else {
+            return true;
+        }
+    }
 }
