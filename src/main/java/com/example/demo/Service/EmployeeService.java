7da118bbedad25567ca7eca6bfd51c5d27060a6e
Abuu001 <abulugz@gmail.com>
2021-06-09 00:32:47 +0300

    services

--- src/main/java/com/example/demo/Service/EmployeeService.java
+++ src/main/java/com/example/demo/Service/EmployeeService.java
@@ -6,9 +6,11 @@ import com.example.demo.Repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 public class EmployeeService {
 
     @Autowired
--- src/main/java/com/example/demo/Service/FlightBookingService.java
+++ src/main/java/com/example/demo/Service/FlightBookingService.java
@@ -1,2 +1,53 @@
-package com.example.demo.Service;public class FlightBookingService {
+package com.example.demo.Service;
+
+import com.example.demo.Entity.PassengerInfo;
+import com.example.demo.Entity.PaymentInfo;
+import com.example.demo.Repository.PassengerInfoRepository;
+import com.example.demo.Repository.PaymentInfoRepository;
+import com.example.demo.Utils.PaymentUtils;
+import com.example.demo.dto.FlightBookingAcknowledgement;
+import com.example.demo.dto.FlightBookingRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class FlightBookingService {
+
+    @Autowired
+    private PassengerInfoRepository passengerInfoRepository;
+
+    @Autowired
+    private PaymentInfoRepository paymentInfoRepository;
+
+    @Transactional //when there is an error at one point  << Rollback (dont save in Db)
+    public FlightBookingAcknowledgement bookFlightTicket(FlightBookingRequest request){
+        FlightBookingAcknowledgement acknowledgement =null;
+
+        PassengerInfo passengerInfo = request.getPassengerInfo();
+        passengerInfo=passengerInfoRepository.save(passengerInfo);
+
+        PaymentInfo paymentInfo = request.getPaymentInfo();
+
+        //validating the amount of money
+        PaymentUtils.validateCreditLimit(paymentInfo.getAccountNo() , passengerInfo.getFare());
+
+        paymentInfo.setPassengerId(passengerInfo.getPId());
+        paymentInfo.setAmount(passengerInfo.getFare());
+
+        paymentInfoRepository.save(paymentInfo);
+
+        return new FlightBookingAcknowledgement("SUCCESS",passengerInfo.getFare(), UUID.randomUUID().toString().split("-")[0],passengerInfo);
+    }
+
+    public List<PassengerInfo> allpassengers() {
+        return passengerInfoRepository.findAll();
+    }
+
+    public List<PaymentInfo> allpayments() {
+        return paymentInfoRepository.findAll();
+    }
 }
