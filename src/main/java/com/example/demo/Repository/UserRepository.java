c095e0f747a89ac7a9ad4f24c9140e6d64b63f46
Abuu001 <abulugz@gmail.com>
2021-06-14 22:38:34 +0300

    user repo to fetch details from db

--- src/main/java/com/example/demo/Repository/UserRepository.java
+++ src/main/java/com/example/demo/Repository/UserRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User,Long>{
 
     User findByUsername(String username);
+    String findByEmail(String email);
 }
