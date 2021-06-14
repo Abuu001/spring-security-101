6d1da3a77819b37407541c3fa245f1bca895ea35
Abuu001 <abulugz@gmail.com>
2021-06-14 22:36:25 +0300

    Configured routes,registered jwt tokens stateless

--- src/main/java/com/example/demo/Security/WebSecurityConfig.java
+++ src/main/java/com/example/demo/Security/WebSecurityConfig.java
@@ -1,8 +1,55 @@
 package com.example.demo.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider =new DaoAuthenticationProvider();
+
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+
+        return provider;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+             http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/")
+                  .permitAll()
+                .antMatchers("/home")
+                  .hasAuthority("USER")
+                .antMatchers("/admin")
+                  .hasAuthority("ADMIN")
+                .antMatchers("/user/save","/user/login")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // bcoz we r using jwt whiich is stateless
+               // .and()
+               // .httpBasic();
+    }
 }
