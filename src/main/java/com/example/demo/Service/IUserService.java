17b1bf123039f11dc3ef2d63c49a15d5e9e41db4
Abuu001 <abulugz@gmail.com>
2021-06-14 22:35:39 +0300

    added findByEmail function

--- src/main/java/com/example/demo/Service/IUserService.java
+++ src/main/java/com/example/demo/Service/IUserService.java
@@ -1,2 +1,9 @@
-package com.example.demo.Service;public interface IUserService {
+package com.example.demo.Service;
+
+import com.example.demo.Entity.User;
+import com.example.demo.Exception.EmailAlreadyExistsException;
+
+public interface IUserService {
+    Long saveUser(User user) throws EmailAlreadyExistsException;
+
 }
