a4b11794e35f34904c58faa451968c8d7f35e75a
Abuu001 <abulugz@gmail.com>
2021-06-14 22:38:00 +0300

    configured func to save users to db

--- src/main/java/com/example/demo/Service/UserServiceImpl.java
+++ src/main/java/com/example/demo/Service/UserServiceImpl.java
@@ -1,2 +1,32 @@
-package com.example.demo.Service;public class UserServiceImpl {
+package com.example.demo.Service;
+
+import com.example.demo.Entity.User;
+import com.example.demo.Exception.EmailAlreadyExistsException;
+import com.example.demo.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements IUserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder pwdEncoder;
+
+    @Override
+    public Long saveUser(User user) throws EmailAlreadyExistsException {
+        //TODO : Encode password
+        user.setPassword(pwdEncoder.encode(user.getPassword()));
+
+        String email = userRepository.findByEmail(user.getEmail());
+
+        if(user.getEmail().equals(email)){
+            throw new EmailAlreadyExistsException("Email is already taken 🚫🚫🚫");
+        }
+
+        return userRepository.save(user).getId();
+    }
 }
