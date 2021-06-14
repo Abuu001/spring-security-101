be9f8e44cdbe9b2d6e596a1b1e4c26f1f994c612
Abuu001 <abulugz@gmail.com>
2021-06-14 22:34:22 +0300

    user properties

--- src/main/java/com/example/demo/Entity/User.java
+++ src/main/java/com/example/demo/Entity/User.java
@@ -4,25 +4,48 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
 
 @Entity
-@Table(name = "user_Login")
+@Table(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
-    private String username;
-    private String password;
-    private String role;
+    private String name;
 
-    public User(String username, String password, String role) {
+    @NotBlank
+    private String username;
+
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    @Size(min = 5)
+    private String password;
+
+    @ElementCollection // stores this element to a separate table
+    @CollectionTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id")
+    ) //The @CollectionTable annotation is used to specify the name of the table that stores all the records of the collection, and the JoinColumn that refers to the primary table.
+    @Column(name = "role")
+    private Set<String> role;
+
+    public User(String name, String username, String email, String password, Set<String> role) {
+        this.name = name;
         this.username = username;
+        this.email = email;
         this.password = password;
         this.role = role;
     }
