27e81736c37175c4a6260263f3e5f73a6b7f1ba3
Abuu001 <abulugz@gmail.com>
2021-06-14 22:31:15 +0300

    Token response after successful login

--- src/main/java/com/example/demo/dto/UserResponse.java
+++ src/main/java/com/example/demo/dto/UserResponse.java
@@ -1,2 +1,14 @@
-package com.example.demo.dto;public class UserResponse {
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+
+    private String token;
+    private String message;
 }
