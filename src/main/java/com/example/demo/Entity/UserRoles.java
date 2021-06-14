e415149f674e066dc653154fd7bc77761c5a34fe
Abuu001 <abulugz@gmail.com>
2021-06-14 22:34:49 +0300

    enum for unique roles a user can have

--- src/main/java/com/example/demo/Entity/UserRoles.java
+++ src/main/java/com/example/demo/Entity/UserRoles.java
@@ -1,2 +1,18 @@
-package com.example.demo.Entity;public enum UserRoles {
+package com.example.demo.Entity;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRoles {
+    ADMIN("ADMIN"),
+    USER("USER"),
+    GUEST("GUEST");
+
+    private final String role;
+
+    UserRoles(String role) {
+        this.role = role;
+    }
+
+
 }
