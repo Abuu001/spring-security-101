f6e2634a9f4d602dfee3c89e6f99b850c9c85661
Abuu001 <abulugz@gmail.com>
2021-06-14 22:34:05 +0300

    function to generate tokens

--- src/main/java/com/example/demo/Utils/JwtUtil.java
+++ src/main/java/com/example/demo/Utils/JwtUtil.java
@@ -1,2 +1,61 @@
-package com.example.demo.Utils;public class JwtUtil {
+package com.example.demo.Utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class JwtUtil {
+
+    @Value("${app.JWTSECRET}")
+    private String secret;
+
+    //generate Token
+    public String generateToken(String subject){
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuer("Abraham")
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(15)))
+                .signWith(SignatureAlgorithm.HS256,secret.getBytes())
+                .compact();
+    }
+
+    //read claims
+    public Claims getClaims(String token){
+        return  Jwts.parser()
+                .setSigningKey(secret.getBytes())
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    //Read exp date
+    public  Date getExpDate(String token){
+        return getClaims(token).getExpiration();
+    }
+
+    //read subject/username
+    public  String getUserName(String token){
+        return getClaims(token).getSubject();
+    }
+
+    // validate Exp date
+    public boolean isTokenExpired(String token){
+        Date expDate = getExpDate(token);
+        return expDate.before(new Date(System.currentTimeMillis()));
+    }
+
+    //validate usename in token and database expdate
+    public  boolean validToken(String token,String username){
+        String tokenUserName =getUserName(token);
+        return (username.equals(tokenUserName) && !isTokenExpired(token));
+    }
+
+
 }
