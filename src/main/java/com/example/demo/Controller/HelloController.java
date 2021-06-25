c821997750e3b98773dbffb72de166f4e9e85309
Abuu001 <abulugz@gmail.com>
2021-06-25 22:26:28 +0300

    controller

--- src/main/java/com/example/demo/Controller/HelloController.java
+++ src/main/java/com/example/demo/Controller/HelloController.java
@@ -1,2 +1,16 @@
-package com.example.demo.Controller;public class HelloController {
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name",defaultValue = "World") String name){
+        return  String.format("Hello , %s",name);
+    }
 }
