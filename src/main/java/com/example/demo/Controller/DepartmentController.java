ab2d0f00d010abf53f259468f6f2d819761b76ea
Abuu001 <abulugz@gmail.com>
2021-06-25 22:36:42 +0300

    department controller

--- src/main/java/com/example/demo/Controller/DepartmentController.java
+++ src/main/java/com/example/demo/Controller/DepartmentController.java
@@ -1,11 +1,30 @@
 package com.example.demo.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.Entity.Department;
+import com.example.demo.Service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping()
+@RequestMapping("/departments")
+@Slf4j
 public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    @PostMapping("/newdepartment")
+    public Department saveDepartment(@RequestBody Department newDepartment){
+        log.info("Inside saveDepartment controller");
+        return  departmentService.saveDepartment(newDepartment);
+    }
+
+    @GetMapping("/find/{departmentId}")
+    public Department findDepartment(@PathVariable("departmentId") Long departmentId){
+        log.info("Inside find department by id -- service");
+        return departmentService.findDepartment(departmentId);
+    }
 }
 
 
