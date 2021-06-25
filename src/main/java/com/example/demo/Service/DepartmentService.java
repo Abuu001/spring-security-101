87ee8252dc509a16560dd027cc03fe2c0df0fd1a
Abuu001 <abulugz@gmail.com>
2021-06-25 22:37:04 +0300

    department service

--- src/main/java/com/example/demo/Service/DepartmentService.java
+++ src/main/java/com/example/demo/Service/DepartmentService.java
@@ -1,2 +1,25 @@
-package com.example.demo.Service;public class DepartmentService {
+package com.example.demo.Service;
+
+import com.example.demo.Entity.Department;
+import com.example.demo.Repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department newDepartment) {
+        log.info("Inside save department --service");
+        return departmentRepository.save(newDepartment);
+    }
+
+    public Department findDepartment(Long departmentId){
+        log.info("Inside find department by id -- service");
+        return departmentRepository.findByDepartmentId(departmentId);
+    }
 }
