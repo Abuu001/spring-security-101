5fd6e535e5d2b1eca57d1408082444e0b36212e0
Abuu001 <abulugz@gmail.com>
2021-06-25 22:32:07 +0300

    test for json strings from controllers

--- src/test/java/com/example/demo/Controller/JsonAssert.java
+++ src/test/java/com/example/demo/Controller/JsonAssert.java
@@ -1,2 +1,28 @@
-package com.example.demo.Controller;public class JsonAssert {
+package com.example.demo.Controller;
+
+import org.json.JSONException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+public class JsonAssert {
+
+    @Test
+    @DisplayName("checks  if the json strings   match")
+    public void shouldReturnJsonString_StrictToTrue() throws JSONException {
+        String actualResponse ="{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}";
+
+        String expectedResponse ="{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}";
+
+        JSONAssert.assertEquals(expectedResponse,actualResponse,true);
+    }
+
+    @Test
+    public void shouldReturnJsonString_StrictToFalse() throws JSONException {
+        String actualResponse ="{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}";
+
+        String expectedResponse ="{\"id\":1,\"name\":\"Ball\"}"; //incomplete json
+
+        JSONAssert.assertEquals(expectedResponse,actualResponse,false); //when 3rd param is false you can have json which is not full(completed)
+    }
 }
