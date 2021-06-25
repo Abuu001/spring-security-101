115983dea08e9f0d184f9a2cab8ebc99bfe24607
Abuu001 <abulugz@gmail.com>
2021-06-25 22:36:02 +0300

    hello world controller

--- src/main/java/com/example/demo/Controller/HelloWorldController.java
+++ src/main/java/com/example/demo/Controller/HelloWorldController.java
@@ -1,2 +1,17 @@
-package com.example.demo.Controller;public class HelloWorldController {
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2")
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "HellWorld2";
+    }
+
+
 }
