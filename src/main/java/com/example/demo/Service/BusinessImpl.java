06702519383dbd2bf9939b723364b4e1a1ac140a
Abuu001 <abulugz@gmail.com>
2021-06-25 22:30:05 +0300

    Business service impl

--- src/main/java/com/example/demo/Service/BusinessImpl.java
+++ src/main/java/com/example/demo/Service/BusinessImpl.java
@@ -1,2 +1,30 @@
-package com.example.demo.Service;public class BusinessImpl {
+package com.example.demo.Service;
+
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Setter
+public class BusinessImpl {
+
+    @Autowired
+    private  DataService dataService;
+
+    public int calculateSum(int[] data){
+        int sum =0;
+
+        for(int value:data){
+            sum +=value;
+        }
+        return sum;
+    }
+
+    public int calculateSumUsingDataService(){
+        int sum =0;
+        int[] data = dataService.retriverAllData();
+
+        for(int value:data){
+            sum +=value;
+        }
+        return sum;
+    }
 }
