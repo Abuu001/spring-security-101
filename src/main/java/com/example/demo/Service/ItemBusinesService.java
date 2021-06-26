2ed6a23a22203b827f50826b46c6e8a82ba357a0
Abuu001 <abulugz@gmail.com>
2021-06-26 22:36:20 +0300

    added item logic

--- src/main/java/com/example/demo/Service/ItemBusinesService.java
+++ src/main/java/com/example/demo/Service/ItemBusinesService.java
@@ -1,2 +1,30 @@
-package com.example.demo.Service;public class ItemBusinesService {
+package com.example.demo.Service;
+
+import com.example.demo.Entity.Item;
+import com.example.demo.Repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemBusinesService {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    public Item retrieveHardCodedItem() {
+        return new Item(2,"Laptop",77,6,4);
+    }
+
+    public List<Item> retrieveAllItems(){
+
+        List<Item> items = itemRepository.findAll();
+
+        for(Item item:items){
+            item.setValue(item.getPrice() * item.getQuantity());
+        }
+
+        return items;
+    }
 }
