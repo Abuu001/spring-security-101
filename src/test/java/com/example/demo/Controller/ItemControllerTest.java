f117c84c6b4835f1dde541d43ca93cb722057193
Abuu001 <abulugz@gmail.com>
2021-06-26 22:37:37 +0300

    tested item controller

--- src/test/java/com/example/demo/Controller/ItemControllerTest.java
+++ src/test/java/com/example/demo/Controller/ItemControllerTest.java
@@ -1,11 +1,14 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Entity.Item;
+import com.example.demo.Service.ItemBusinesService;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,7 +16,10 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class) //JUnit 5
 @WebMvcTest(ItemController.class)
@@ -21,6 +27,9 @@ class ItemControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private ItemBusinesService itemBusinesService;
 
     @Test
     void dummyItem() throws Exception {
@@ -39,4 +48,41 @@ class ItemControllerTest {
 
     }
 
+    @Test
+    void itemFromBusinessService() throws Exception {
+
+        when(itemBusinesService.retrieveHardCodedItem()).thenReturn(new Item(2,"Laptop",77,6,4));
+
+        RequestBuilder request= MockMvcRequestBuilders
+                .get("/item-from-business-service")
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(request)
+                //   .andExpect(status().isOk())
+                 //.andExpect(content().json("{"id":1,"name":"Ball","price":10,"quantity":100}"))
+                .andReturn();
+
+        assertEquals("{\"id\":2,\"name\":\"Laptop\",\"price\":77,\"quantity\":6}",result.getResponse().getContentAsString());
+
+    }
+
+    @Test
+    void allItems() throws Exception {
+
+        when(itemBusinesService.retrieveAllItems()).thenReturn(
+                Arrays.asList(new Item(15,"Pencil",11,20,220))
+        );
+
+        RequestBuilder request= MockMvcRequestBuilders
+                .get("/all-items-from-dabatase")
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(request)
+                //   .andExpect(status().isOk())
+                //.andExpect(content().json("[{"id":15,"name":"Pencil","price":11,"quantity":20,"value":220}]"))
+                .andReturn();
+
+        assertEquals("[{\"id\":15,\"name\":\"Pencil\",\"price\":11,\"quantity\":20,\"value\":220}]",result.getResponse().getContentAsString());
+
+    }
 }
