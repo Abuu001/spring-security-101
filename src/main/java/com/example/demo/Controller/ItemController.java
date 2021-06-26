5c6f4c9c0467075534dae0263b3e6a1216d24504
Abuu001 <abulugz@gmail.com>
2021-06-26 22:34:49 +0300

    added items controller

--- src/main/java/com/example/demo/Controller/ItemController.java
+++ src/main/java/com/example/demo/Controller/ItemController.java
@@ -1,2 +1,32 @@
-package com.example.demo.Controller;public class ItemController {
+package com.example.demo.Controller;
+
+import com.example.demo.Entity.Item;
+import com.example.demo.Service.ItemBusinesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ItemController {
+
+    @Autowired
+    private ItemBusinesService itemBusinesService;
+
+    @GetMapping("/dummy-item")
+    public Item dummyItem(){
+        return new Item(1,"Ball",10,100,4);
+    }
+
+    @GetMapping("/item-from-business-service")
+    public Item itemFromBusinessService(){
+        return itemBusinesService.retrieveHardCodedItem();
+    }
+
+    @GetMapping("/all-items-from-dabatase")
+    public List<Item> allItems(){
+        return itemBusinesService.retrieveAllItems();
+    }
+
 }
---
+++ src/test/java/com/example/demo/Service/ItemBusinesServiceTest.java
@@ -0,0 +1,11 @@
+package com.example.demo.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ItemBusinesServiceTest {
+
+    @Test
+    void retrieveAllItems() {
+    }
+}
