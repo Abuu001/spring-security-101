23132b514d1dc5d4d0fef6c08512324c219e0303
Abuu001 <abulugz@gmail.com>
2021-06-26 22:35:12 +0300

    added Value as transient

--- src/main/java/com/example/demo/Entity/Item.java
+++ src/main/java/com/example/demo/Entity/Item.java
@@ -2,13 +2,30 @@ package com.example.demo.Entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @AllArgsConstructor
 @Data
+@Entity
+@NoArgsConstructor
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private int price;
     private int quantity;
 
+    @Transient
+    private int value;
+
+    public Item(int id, String name, int price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
